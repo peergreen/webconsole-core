5bfe507df5528c313b22e8b9cc5df880f949bf1c
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-07-30 16:05:58 +0200

    Rename class

--- src/main/java/com/peergreen/webconsole/core/handler/ExtensionPointModel.java
+++ src/main/java/com/peergreen/webconsole/core/handler/ExtensionPointModel.java
@@ -5,12 +5,12 @@ import java.lang.reflect.Method;
 /**
  * @author Mohammed Boukada
  */
-public class ExtensionPoint {
+public class ExtensionPointModel {
     private Method bindMethod;
     private Method unbindMethod;
     private String filter;
 
-    public ExtensionPoint(Method bindMethod, Method unbindMethod, String filter) {
+    public ExtensionPointModel(Method bindMethod, Method unbindMethod, String filter) {
         this.bindMethod = bindMethod;
         this.unbindMethod = unbindMethod;
         this.filter = filter;
