9f814401713b476c50a55c49b1206ca2373a889c
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-08-01 11:27:15 +0200

    PGWK-25	 - Navigate between views using URLs : avoid creating new UI when typing in browser's address bar

--- src/main/java/com/peergreen/webconsole/core/osgi/VaadinOSGiServlet.java
+++ src/main/java/com/peergreen/webconsole/core/osgi/VaadinOSGiServlet.java
@@ -3,6 +3,10 @@ package com.peergreen.webconsole.core.osgi;
 import javax.servlet.annotation.WebServlet;
 
 import com.peergreen.webconsole.core.exception.VaadinErrorHandler;
+import com.vaadin.annotations.PreserveOnRefresh;
+import com.vaadin.server.BootstrapFragmentResponse;
+import com.vaadin.server.BootstrapListener;
+import com.vaadin.server.BootstrapPageResponse;
 import com.vaadin.server.DeploymentConfiguration;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.SessionInitEvent;
@@ -10,6 +14,8 @@ import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.UIProvider;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinServletService;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Vaadin OSGi Servlet
@@ -18,6 +24,8 @@ import com.vaadin.server.VaadinServletService;
  */
 @WebServlet(asyncSupported=true)
 public class VaadinOSGiServlet extends VaadinServlet {
+
+    private static final long serialVersionUID = 1L;
 
     /**
      * Vaadin UI provider
@@ -47,6 +55,32 @@ public class VaadinOSGiServlet extends VaadinServlet {
                 // Add Vaadin UI provider to the Vaadin session
                 e.getSession().addUIProvider(provider);
                 e.getSession().setErrorHandler(new VaadinErrorHandler());
+                e.getSession().addBootstrapListener(new BootstrapListener() {
+                    @Override
+                    public void modifyBootstrapFragment(BootstrapFragmentResponse response) {
+
+                    }
+
+                    /*
+                     * Ensure window has "stable name", in case PreserveOnRefresh is used.
+                     * This is to fool vaadinBootstrap.js so that for example applications
+                     * used as ios "home screen webapps", can preserve their state among app
+                     * switches, like following links (with browser) and then returning back
+                     * to app.
+                     */
+                    @Override
+                    public void modifyBootstrapPage(BootstrapPageResponse response) {
+                        Document document = response.getDocument();
+                        Element head = document.getElementsByTag("head").get(0);
+                        Element element;
+                        if (response.getUiClass().getAnnotation(PreserveOnRefresh.class) != null) {
+                            element = document.createElement("script");
+                            element.attr("type", "text/javascript");
+                            element.appendText("\nwindow.name = '" + response.getUiClass().hashCode() + "';\n");
+                            head.appendChild(element);
+                        }
+                    }
+                });
             }
          });
 
