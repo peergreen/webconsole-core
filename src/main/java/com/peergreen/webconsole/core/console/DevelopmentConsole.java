ed293b61a31ca52ef7e6c895af0dc3aa7b162f66
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-09-26 15:46:35 +0200

    Console factories

---
+++ src/main/java/com/peergreen/webconsole/core/console/DevelopmentConsole.java
@@ -0,0 +1,31 @@
+/**
+ * Peergreen S.A.S. All rights reserved.
+ * Proprietary and confidential.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.peergreen.webconsole.core.console;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.StaticServiceProperty;
+
+import com.peergreen.webconsole.Constants;
+import com.peergreen.webconsole.IConsole;
+
+/**
+ * Peergreen Administration Unsecured Console
+ *
+ * @author Mohammed Boukada
+ */
+@Component(name = Constants.DEVELOPMENT_MODE_CONSOLE_PID)
+@Provides(properties = {@StaticServiceProperty(name = Constants.CONSOLE_NAME, type = "java.lang.String", mandatory = true),
+        @StaticServiceProperty(name = Constants.CONSOLE_ALIAS, type = "java.lang.String", mandatory = true),
+        @StaticServiceProperty(name = Constants.DEFAULT_ROLES, type = "java.lang.String[]", mandatory = false)})
+public class DevelopmentConsole implements IConsole {
+}
---
+++ src/main/java/com/peergreen/webconsole/core/console/ProductionConsole.java
@@ -0,0 +1,30 @@
+/**
+ * Peergreen S.A.S. All rights reserved.
+ * Proprietary and confidential.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.peergreen.webconsole.core.console;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.StaticServiceProperty;
+
+import com.peergreen.webconsole.Constants;
+import com.peergreen.webconsole.IConsole;
+
+/**
+ * Peergreen Administration Secured Console
+ *
+ * @author Mohammed Boukada
+ */
+@Component(name = Constants.PRODUCTION_MODE_CONSOLE_PID)
+@Provides(properties = {@StaticServiceProperty(name = Constants.CONSOLE_NAME, type = "java.lang.String", mandatory = true),
+        @StaticServiceProperty(name = Constants.CONSOLE_ALIAS, type = "java.lang.String", mandatory = true)})
+public class ProductionConsole implements IConsole {
+}
